d7a2137bff78b950c44f82ddcb793ec783aea08b
SaiMani-Ritish <maniritish2422@gmail.com>
2025-02-09 23:24:09 +0000

    PE03 - Submission

---
+++ PE03 - Todo List/todolist/README.md
@@ -0,0 +1,16 @@
+# ToDo List App
+
+## Input
+- The user can input a task description in the input field.
+- By clicking the "Add Task" button we can add the task to the list.
+- The user can click the "Delete" button next to a task to remove a task from the list.
+
+## Process
+- The app is using React's `useState` hook to manage the state of the todos and the input value.
+- When the "Add Task" button is clicked, then the input value is added to the `todos` array.
+- When the "Delete" button is clicked, the task is removed from the `todos` array.
+- The `.map()` function dynamically renders the list of todos.
+
+## Output
+- The app displays the list of tasks that are added.
+- Each task has a "Delete" button that can removes it from the list.
---
+++ PE03 - Todo List/todolist/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleAddTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>ToDo List</h1>
+      <div className="todo-container">
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder="Enter a task"
+        />
+        <button onClick={handleAddTodo}>Add Task</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
